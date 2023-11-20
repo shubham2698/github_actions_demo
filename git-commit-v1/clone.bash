5527dc2e0ff414b45f0890cd33a3f87866ba1980
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-11-20 11:48:14 +0530

    restore changes

--- git-commit-v1/clone.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "${{ inputs.repository }}" != "." ]; then
-    git clone https://${{ inputs.token }}@github.com/${{ inputs.repository }}.git
-    cd ${{ inputs.repository-name }}
-    git checkout ${{ inputs.branch }}
-          
-fi
