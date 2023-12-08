bdb034cc92b8bdfe926897b1058f1f3b049bcbb1
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-12-09 00:19:53 +0530

    taskbase automation

--- selenium-taskbase/taskbase.py
+++ selenium-taskbase/taskbase.py
@@ -2,21 +2,9 @@ from setup_selenium import setup
 from steps import login,select_project,select_phase,select_time,write_description,add_data
 from get_data import get_data
 
-import selenium.webdriver as webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import WebDriverException
-
 url_to_open = 'http://114.143.149.14:4356//TaskbaseAWS-2.0/NeovaTaskBase/LoginPage.aspx'
 file_path = './data.csv'
-try:
-    gecko_driver_path = 'geckodriver'
-    firefox_service = Service(gecko_driver_path)
-    firefox_options = Options()
-    # firefox_options.headless = True
-    browser = webdriver.Firefox(service=firefox_service,options=firefox_options)
-except WebDriverException as e:
-        print(f"WebDriverException: {e}")
+browser = setup()
 
 try:
     
