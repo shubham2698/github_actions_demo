d512da382a06141815ef8b1f8767d24643f62c01
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-12-09 00:02:24 +0530

    taskbase automation

--- selenium-taskbase/setup_selenium.py
+++ selenium-taskbase/setup_selenium.py
@@ -4,9 +4,12 @@ from selenium.webdriver.firefox.options import Options
 
 
 def setup():
-    gecko_driver_path = 'geckodriver'
-    firefox_service = Service(gecko_driver_path)
-    firefox_options = Options()
-    firefox_options.headless = True
-    browser = webdriver.Firefox(service=firefox_service,options=firefox_options)
-    return browser
+    try:
+        gecko_driver_path = 'selenium-taskbase/geckodriver'
+        firefox_service = Service(gecko_driver_path)
+        firefox_options = Options()
+        firefox_options.headless = True
+        browser = webdriver.Firefox(service=firefox_service,options=firefox_options)
+        return browser
+    except webdriver.WebDriverException as e:
+        print(f"WebDriverException: {e}")
