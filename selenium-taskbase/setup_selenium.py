4be68a27b714708d21e6df1504133c13f60e8701
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-12-08 23:06:05 +0530

    taskbase automation

--- selenium-taskbase/setup_selenium.py
+++ selenium-taskbase/setup_selenium.py
@@ -4,8 +4,9 @@ from selenium.webdriver.firefox.options import Options
 
 
 def setup():
-    gecko_driver_path = 'selenium-taskbase/geckodriver'
+    gecko_driver_path = './selenium-taskbase/geckodriver'
     firefox_service = Service(gecko_driver_path)
     firefox_options = Options()
+    firefox_options.headless = True
     browser = webdriver.Firefox(service=firefox_service,options=firefox_options)
     return browser
