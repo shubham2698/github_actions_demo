92f055826614f46ff103e0466290d844c52e5d4d
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-12-11 19:41:52 +0530

    requirement.txt updated

--- selenium-taskbase/taskbase.py
+++ selenium-taskbase/taskbase.py
@@ -4,7 +4,7 @@ import selenium.webdriver as webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import WebDriverException
-from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 import pytest
 
@@ -12,7 +12,7 @@ import pytest
 def browser():
     try:
         
-        gecko_driver_path = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        gecko_driver_path = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         firefox_service = Service(gecko_driver_path)
         firefox_options = Options()
         firefox_options.headless = True
